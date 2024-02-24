f2ed7123c481792d43542a33ee972c1a6fbddd83
Benji Huser-Berta <benj.huser@gmail.com>
2024-02-24 12:47:11 +0100

    Remove unused code in PrivacyModel

--- CMFTAspNet/Pages/Privacy.cshtml.cs
+++ CMFTAspNet/Pages/Privacy.cshtml.cs
@@ -1,19 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CMFTAspNet.Pages;
 
 public class PrivacyModel : PageModel
 {
-    private readonly ILogger<PrivacyModel> _logger;
-
-    public PrivacyModel(ILogger<PrivacyModel> logger)
-    {
-        _logger = logger;
-    }
-
-    public void OnGet()
-    {
-    }
 }
 
