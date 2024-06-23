8b66be0b72da9f1dd6aab941d92c452c100dfc45
Benji Huser-Berta <benj.huser@gmail.com>
2024-06-23 09:51:03 +0200

    Add Projects Controller

---
+++ Lighthouse.Tests/API/ProjectsControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using Lighthouse.API;
+using Lighthouse.Models;
+using Lighthouse.Services.Interfaces;
+using Moq;
+
+namespace Lighthouse.Tests.API
+{
+    public class ProjectsControllerTest
+    {
+        private Mock<IRepository<Project>> projectRepoMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            projectRepoMock = new Mock<IRepository<Project>>();
+        }
+
+        [Test]
+        public async Task GetProjects_ReturnsAllProjectsFromRepository()
+        {
+            var testProjects = GetTestProjects();
+            projectRepoMock.Setup(x => x.GetAll()).Returns(testProjects);
+
+            var subject = CreateSubject();
+
+            var result = await subject.Get();
+
+            Assert.That(result.Count, Is.EqualTo(testProjects.Count()));
+        }
+
+        private ProjectsController CreateSubject()
+        {
+            return new ProjectsController(projectRepoMock.Object);
+        }
+
+        private IEnumerable<Project> GetTestProjects()
+        {
+            return new List<Project>
+            {
+                new Project { Id = 12, Name = "Foo" },
+                new Project { Id = 42, Name = "Bar" }
+            };
+        }
+    }
+}
---
+++ Lighthouse/API/ProjectsController.cs
@@ -0,0 +1,30 @@
+﻿using Lighthouse.Models;
+using Lighthouse.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lighthouse.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly IRepository<Project> repository;
+
+        public ProjectsController(IRepository<Project> repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Project>> Get()
+        {
+            return await Task.FromResult(repository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return NotFound();
+        }
+    }
+}
