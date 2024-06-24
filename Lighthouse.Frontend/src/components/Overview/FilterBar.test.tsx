cbc83a197a43b249e42528a148d25df9557bdb94
Benji Huser-Berta <benj.huser@gmail.com>
2024-06-24 09:00:33 +0200

    Add unit tests for FilterBar

---
+++ Lighthouse.Frontend/src/components/Overview/FilterBar.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { describe, expect } from 'vitest';
+import FilterBar from './FilterBar';
+
+describe('FilterBar', () => {
+  test('renders the input with the correct placeholder and value', () => {
+    const filterText = "test";
+    const onFilterTextChange = vi.fn();
+
+    render(<FilterBar filterText={filterText} onFilterTextChange={onFilterTextChange} />);
+
+    const inputElement = screen.getByPlaceholderText('Search...');
+    expect(inputElement).toBeInTheDocument();
+    expect(inputElement).toHaveValue(filterText);
+  });
+
+  test('calls onFilterTextChange when the input value changes', () => {
+    const filterText = "test";
+    const onFilterTextChange = vi.fn();
+
+    render(<FilterBar filterText={filterText} onFilterTextChange={onFilterTextChange} />);
+
+    // Simulate user typing in the input
+    const inputElement = screen.getByPlaceholderText('Search...');
+    fireEvent.change(inputElement, { target: { value: 'new text' } });
+
+    // Check if onFilterTextChange is called with the correct value
+    expect(onFilterTextChange).toHaveBeenCalledTimes(1);
+    expect(onFilterTextChange).toHaveBeenCalledWith('new text');
+  });
+});
--- Lighthouse.Frontend/vite.config.ts
+++ Lighthouse.Frontend/vite.config.ts
@@ -1,12 +1,20 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: ["./setupTests.ts"],
+        css: true,
+      },
     build: {
         outDir: '../Lighthouse/wwwroot',
         emptyOutDir: false,
         minify: true,
     },
-    base: '/', // Ensure the base path matches the subfolder
+    base: '/'
 });
