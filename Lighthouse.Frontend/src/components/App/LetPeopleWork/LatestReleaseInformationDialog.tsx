331dce09b33251c16699dbd7ce86bf295c1316f5
Benji Huser-Berta <benj.huser@gmail.com>
2024-08-17 08:22:47 +0200

    Add update details to new version available dialog

--- Lighthouse.Frontend/src/components/App/LetPeopleWork/LatestReleaseInformationDialog.tsx
+++ Lighthouse.Frontend/src/components/App/LetPeopleWork/LatestReleaseInformationDialog.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Dialog, DialogContent, DialogActions, Button, Typography, Grid, List, ListItem, Link as MuiLink, DialogTitle } from '@mui/material';
+import { Dialog, DialogContent, DialogActions, Button, Typography, Grid, List, ListItem, Link as MuiLink, DialogTitle, Box } from '@mui/material';
 import { ILighthouseRelease } from '../../../models/LighthouseRelease/LighthouseRelease';
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import InputGroup from '../../Common/InputGroup/InputGroup';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import DownloadIcon from '@mui/icons-material/Download';
+import UpdateIcon from '@mui/icons-material/Update';
 
 interface LatestReleaseInformationDialogProps {
     open: boolean;
@@ -15,10 +17,24 @@ interface LatestReleaseInformationDialogProps {
 const LatestReleaseInformationDialog: React.FC<LatestReleaseInformationDialogProps> = ({ open, onClose, newReleases }) => {
     return (
         <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
-            <DialogTitle>Newer Versions Available</DialogTitle>
+            <DialogTitle>Update Available</DialogTitle>
             <DialogContent>
+                <Box display="flex" alignItems="center" mb={2}>
+                    <DownloadIcon color="primary" sx={{ marginRight: 1 }} />
+                    <Typography variant="body1">
+                        To update Lighthouse, stop the running instance, download the latest version, and extract it into your Lighthouse folder. Your database and configuration will not be changed by updating.
+                    </Typography>
+                </Box>
+                
+                <Box display="flex" alignItems="center" mb={3}>
+                    <UpdateIcon color="secondary" sx={{ marginRight: 1 }} />
+                    <Typography variant="body1">
+                        You can also use the update script for your respective operating system in your Lighthouse folder (for example, <code>update_windows.ps1</code>). Running this will automatically download and extract the latest version for you.
+                    </Typography>
+                </Box>
+
                 {newReleases && newReleases.map((release, index) => (
-                    <InputGroup key={index} title={release.name} initiallyExpanded={index == 0}>
+                    <InputGroup key={index} title={release.name} initiallyExpanded={index === 0}>
                         <Grid item xs={12}>
                             <Typography variant='body2'>
                                 <Markdown remarkPlugins={[remarkGfm]}>
@@ -40,7 +56,7 @@ const LatestReleaseInformationDialog: React.FC<LatestReleaseInformationDialogPro
                             <Typography variant='body1' marginTop={2}>
                                 <GitHubIcon />
                                 <MuiLink href={release.link} target="_blank" rel="noopener noreferrer" marginLeft={2}>
-                                    See GitHub for more Details
+                                    See GitHub for more details
                                 </MuiLink>
                             </Typography>
                         </Grid>
