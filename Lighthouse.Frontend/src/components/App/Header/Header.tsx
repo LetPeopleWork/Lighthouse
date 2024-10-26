91fca90fda09d1cf840c95d65373a596346ab151
Benji Huser-Berta <benj.huser@gmail.com>
2024-10-26 15:07:29 +0200

    Add Initial list of contributors

--- Lighthouse.Frontend/src/components/App/Header/Header.tsx
+++ Lighthouse.Frontend/src/components/App/Header/Header.tsx
@@ -7,6 +7,7 @@ import { BugReport } from '@mui/icons-material';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import BlogIcon from '@mui/icons-material/RssFeed';
+import HandshakeIcon from '@mui/icons-material/Handshake';
 
 const Header: React.FC = () => {
   return (
@@ -23,20 +24,29 @@ const Header: React.FC = () => {
         </Box>
         <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
           <ExternalLinkButton
+            link="https://github.com/LetPeopleWork/Lighthouse/blob/main/CONTRIBUTORS.md"
+            icon={HandshakeIcon}
+            tooltip='Contributors'
+          />
+          <ExternalLinkButton
             link="https://github.com/LetPeopleWork/Lighthouse/issues"
             icon={BugReport}
+            tooltip='Report an Issue'
           />
           <ExternalLinkButton
             link="https://www.youtube.com/channel/UCipDDn2dpVE3rpoKNW2asZQ"
             icon={YouTubeIcon}
+            tooltip='Check out our YouTube Channel'
           />
           <ExternalLinkButton
             link="https://www.letpeople.work/blog/"
             icon={BlogIcon}
+            tooltip='Read our Blog'
           />
           <ExternalLinkButton
             link="https://github.com/LetPeopleWork/"
             icon={GitHubIcon}
+            tooltip='View our OpenSource Projects on GitHub'
           />
         </Box>
       </Toolbar>
