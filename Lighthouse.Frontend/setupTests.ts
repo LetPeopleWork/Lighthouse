e0180c5ccbc0e72c417f8a541771d0b5c33a0ee4
Benji Huser-Berta <benj.huser@gmail.com>
2024-11-22 13:37:54 +0100

    Add MockResizeObserver for unit tests

--- Lighthouse.Frontend/setupTests.ts
+++ Lighthouse.Frontend/setupTests.ts
@@ -2,3 +2,11 @@ import '@testing-library/jest-dom';
 import { registerLicense } from '@syncfusion/ej2-base';
 
 registerLicense('ORg4AjUWIQA/Gnt2UlhhQlVMfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTX9Td0RjUHxcc3xTQ2Bd');
+
+class MockResizeObserver {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  }
+  
+  global.ResizeObserver = MockResizeObserver as any;
