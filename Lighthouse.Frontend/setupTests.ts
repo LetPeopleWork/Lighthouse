d90f1700f5cfed3417e5cf5274d866f8cbdb3917
Benji Huser-Berta <benj.huser@gmail.com>
2024-11-22 13:43:33 +0100

    Fix sonar lint comment

--- Lighthouse.Frontend/setupTests.ts
+++ Lighthouse.Frontend/setupTests.ts
@@ -4,9 +4,9 @@ import { registerLicense } from '@syncfusion/ej2-base';
 registerLicense('ORg4AjUWIQA/Gnt2UlhhQlVMfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTX9Td0RjUHxcc3xTQ2Bd');
 
 class MockResizeObserver {
-    observe() {}
-    unobserve() {}
-    disconnect() {}
+    observe() { /* Just declared to fulfill the interface */ }
+    unobserve() { /* Just declared to fulfill the interface */ }
+    disconnect() { /* Just declared to fulfill the interface */ }
   }
   
   global.ResizeObserver = MockResizeObserver as any;
