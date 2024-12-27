ad44cf281fe41441ee0a5a14d6b3892b36c70e9b
Benji Huser-Berta <benj.huser@gmail.com>
2024-12-27 14:38:08 +0100

    Add tests for TeamDetailPage

--- Lighthouse.EndToEndTests/tests/models/teams/TeamDetailPage.ts
+++ Lighthouse.EndToEndTests/tests/models/teams/TeamDetailPage.ts
@@ -1,10 +1,41 @@
 import { Locator, Page } from '@playwright/test';
+import { TeamEditPage } from './TeamEditPage';
 
 export class TeamDetailPage {
     page: Page;
 
     constructor(page: Page) {
         this.page = page;
+    }
+
+    async updateTeamData(): Promise<void> {
+        await this.updateTeamDataButton.click();
+    }
+
+    async editTeam(): Promise<TeamEditPage> {
+        await this.editTeamButton.click();
+
+        return new TeamEditPage(this.page);
+    }
+
+    async forecast(howMany: number): Promise<number> {
+        await this.page.getByLabel('Number of Items to Forecast').fill(`${howMany}`);
+        await this.page.getByRole('button', { name: 'Forecast' }).click();
+
+        const likelihood = await this.page.getByRole('heading', { name: '%' }).textContent() ?? '0';        
+        const parsedLikelihood = parseFloat(likelihood.replace('%', ''));
+
+        return parsedLikelihood;
+    }
+
+    async getLastUpdatedDate(): Promise<Date> {
+        const lastUpdatedText = await this.page.getByRole('heading', { name: /^Last Updated/ }).textContent() ?? '';
+        const dateMatch = /Last Updated on (.*)/.exec(lastUpdatedText);
+        if (!dateMatch) {
+            return new Date();
+        }
+
+        return new Date(dateMatch[1]);
     }
 
     get updateTeamDataButton(): Locator {
---
+++ Lighthouse.EndToEndTests/tests/specs/TeamsDetail.spec.ts
@@ -0,0 +1,63 @@
+import { expect, test, testWithData } from '../fixutres/LighthouseFixture';
+
+[
+    { name: "Azure DevOps", index: 0 },
+    { name: "Jira", index: 2 }
+]
+    .forEach(({ index, name }) => {
+        testWithData(`should update Team Data for ${name} team on click`, async ({ testData, overviewPage }) => {
+            test.slow();
+            const team = testData.teams[index];
+
+            const teamsPage = await overviewPage.lightHousePage.goToTeams();
+            const teamDetailPage = await teamsPage.goToTeam(team);
+
+            await expect(teamDetailPage.updateTeamDataButton).toBeEnabled();
+
+            await teamDetailPage.updateTeamData();
+            await expect(teamDetailPage.updateTeamDataButton).toBeDisabled();
+
+            // Wait for update to be done
+            await expect(teamDetailPage.updateTeamDataButton).toBeEnabled();
+
+            // TODO: Fix this in Frontend to update page automatically!
+            await teamDetailPage.page.reload();
+
+            const lastUpdatedDate = await teamDetailPage.getLastUpdatedDate();
+            const now = new Date();
+
+            // Make sure lastUpdated is within the last 10 seconds
+            expect(Math.abs(lastUpdatedDate.getUTCMilliseconds() - now.getUTCMilliseconds())).toBeLessThanOrEqual(10000);
+        });
+    });
+
+testWithData('should open Team Edit Page when clicking on Edit Button', async ({ testData, overviewPage }) => {
+    const [team] = testData.teams;
+
+    const teamsPage = await overviewPage.lightHousePage.goToTeams();
+    const teamDetailPage = await teamsPage.goToTeam(team);
+
+    const teamEditPage = await teamDetailPage.editTeam();
+    expect(teamEditPage.page.url()).toContain(`/teams/edit/${team.id}`);
+});
+
+testWithData(`should show Manual When and How Many Forecast for team`, async ({ testData, overviewPage }) => {
+    test.slow();
+    const team = testData.teams[0];
+
+    const teamsPage = await overviewPage.lightHousePage.goToTeams();
+    const teamDetailPage = await teamsPage.goToTeam(team);
+
+    await expect(teamDetailPage.updateTeamDataButton).toBeEnabled();
+
+    await teamDetailPage.updateTeamData();
+    await expect(teamDetailPage.updateTeamDataButton).toBeDisabled();
+
+    // Wait for update to be done
+    await expect(teamDetailPage.updateTeamDataButton).toBeEnabled();
+
+    const howMany = 20;
+    const likelihood = await teamDetailPage.forecast(howMany);
+
+    expect(likelihood).toBeGreaterThan(0);
+});
