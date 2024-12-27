c1f9c62ee9f47d7157c3a70ab92391b7983748ad
Benji Huser-Berta <benj.huser@gmail.com>
2024-12-27 17:06:23 +0100

    Use as many workers as availble to run e2e tests (even on ci)

--- Lighthouse.EndToEndTests/playwright.config.ts
+++ Lighthouse.EndToEndTests/playwright.config.ts
@@ -41,11 +41,13 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  
+  workers: undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: TestConfig.LighthouseUrl,
@@ -55,6 +57,7 @@ export default defineConfig({
     video: 'retain-on-failure',
   },
 
+  /* Set longer timeouts as we depend for some tests on 3rd party software we don't have control over (ADO/Jira) */
   expect: {
     timeout: 15000
   },
