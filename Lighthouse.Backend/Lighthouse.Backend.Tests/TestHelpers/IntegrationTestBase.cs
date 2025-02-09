96b32cb885826d2229de053dde881a7e9817a0b5
Benji Huser-Berta <benj.huser@gmail.com>
2025-02-09 11:48:28 +0100

    Add initial support for postgres
    Adjust migrations so they are in dedicated projects
    Add migrations for Postgres and SQLite
    Allow to configure settings in appsettings (switch between postgres and sqlite)

---
+++ Lighthouse.Backend/Lighthouse.Backend.Tests/TestHelpers/IntegrationTestBase.cs
@@ -0,0 +1,54 @@
+﻿using Lighthouse.Backend.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lighthouse.Backend.Tests.TestHelpers
+{
+    [TestFixture]
+    public abstract class IntegrationTestBase
+    {
+        private readonly TestWebApplicationFactory<Program> webApplicationFactory;
+
+        private IServiceScope serviceScope;
+
+        protected IntegrationTestBase(TestWebApplicationFactory<Program> webApplicationFactory)
+        {
+            this.webApplicationFactory = webApplicationFactory;
+        }
+
+        protected IServiceProvider ServiceProvider { get; private set; }
+
+        protected LighthouseAppContext DatabaseContext { get; private set; }
+
+        [OneTimeTearDown]
+        public void GlobalTearDown()
+        {
+            webApplicationFactory.Dispose();
+        }
+
+        [SetUp]
+        protected void Init()
+        {
+            serviceScope = webApplicationFactory.Services.CreateScope();
+            ServiceProvider = serviceScope.ServiceProvider;
+
+            DatabaseContext = ServiceProvider.GetService<LighthouseAppContext>()
+                ?? throw new InvalidOperationException("Could not Find DB Context");
+
+            // Only use Migrate() as it handles both creation and migration
+            DatabaseContext.Database.Migrate();
+        }
+
+        [TearDown]
+        protected void TearDown()
+        {
+            // Ensure we clean up before disposing
+            if (DatabaseContext != null)
+            {
+                DatabaseContext.Database.EnsureDeleted();
+            }
+
+            serviceScope?.Dispose();
+        }
+    }
+}
