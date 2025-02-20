2ad89ebfaa8278c32d5de2ec3bdb188ee2c94c7c
Benji Huser-Berta <benj.huser@gmail.com>
2025-02-20 20:40:18 +0100

    Adjust building of demo app to use correct out folder

--- Lighthouse.Frontend/vite.config.ts
+++ Lighthouse.Frontend/vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     build: {
-        outDir: '../Lighthouse.Backend/wwwroot',
+        outDir: '../Lighthouse.Backend/Lighthouse.Backend/wwwroot',
         emptyOutDir: false,
         minify: true,
     },
