23b76c539a9915ba0cbed274c324df3285f3b396
Benjamin Huser-Berta <benj.huser@gmail.com>
2025-04-21 22:04:29 +0200

    Make Lighthouse a PWA that is installable

--- Lighthouse.Frontend/vite.config.ts
+++ Lighthouse.Frontend/vite.config.ts
@@ -2,9 +2,35 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'icons/*.png'],
+            manifest: {
+                name: 'Lighthouse',
+                short_name: 'Lighthouse',
+                description: 'Lighthouse Frontend Application',
+                theme_color: '#30574e',
+                icons: [
+                    {
+                        src: '/icons/icon-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/icons/icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable'
+                    }
+                ]
+            }
+        })
+    ],
     build: {
         outDir: '../Lighthouse.Backend/Lighthouse.Backend/wwwroot',
         emptyOutDir: false,
