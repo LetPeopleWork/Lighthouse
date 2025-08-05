6594d3ef6650efbf0c536480f6e935d109cc25ca
Benji Huser-Berta <github.com.bok@huser-berta.com>
2025-08-05 14:42:50 +0200

    Prevent continuous updating if we have more than 3 teams and no license

--- Lighthouse.Backend/Lighthouse.Backend.Tests/Services/Implementation/BackgroundServices/Update/TeamUpdaterTest.cs
+++ Lighthouse.Backend/Lighthouse.Backend.Tests/Services/Implementation/BackgroundServices/Update/TeamUpdaterTest.cs
@@ -3,7 +3,9 @@ using Lighthouse.Backend.Models.AppSettings;
 using Lighthouse.Backend.Services.Implementation.BackgroundServices.Update;
 using Lighthouse.Backend.Services.Implementation.WorkTrackingConnectors;
 using Lighthouse.Backend.Services.Interfaces;
+using Lighthouse.Backend.Services.Interfaces.Licensing;
 using Lighthouse.Backend.Services.Interfaces.Repositories;
+using Lighthouse.Backend.Services.Interfaces.TeamData;
 using Lighthouse.Backend.Tests.TestHelpers;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -14,6 +16,8 @@ namespace Lighthouse.Backend.Tests.Services.Implementation.BackgroundServices.Up
     {
         private Mock<IAppSettingService> appSettingServiceMock;
         private Mock<IRepository<Team>> teamRepoMock;
+        private Mock<ITeamDataService> teamDataServiceMock;
+        private Mock<ILicenseService> licenseServiceMock;
 
         private int idCounter = 0;
 
@@ -22,9 +26,13 @@ namespace Lighthouse.Backend.Tests.Services.Implementation.BackgroundServices.Up
         {
             teamRepoMock = new Mock<IRepository<Team>>();
             appSettingServiceMock = new Mock<IAppSettingService>();
+            licenseServiceMock = new Mock<ILicenseService>();
+            teamDataServiceMock = new Mock<ITeamDataService>();
 
             SetupServiceProviderMock(teamRepoMock.Object);
             SetupServiceProviderMock(appSettingServiceMock.Object);
+            SetupServiceProviderMock(licenseServiceMock.Object);
+            SetupServiceProviderMock(teamDataServiceMock.Object);
 
             SetupRefreshSettings(10, 10);
         }
@@ -39,7 +47,7 @@ namespace Lighthouse.Backend.Tests.Services.Implementation.BackgroundServices.Up
 
             await subject.StartAsync(CancellationToken.None);
 
-            Mock.Get(UpdateQueueService).Verify(x => x.EnqueueUpdate(UpdateType.Team, team.Id, It.IsAny<Func<IServiceProvider, Task>>()));
+            teamDataServiceMock.Verify(x => x.UpdateTeamData(team), Times.Once);
         }
 
         [Test]
@@ -53,8 +61,8 @@ namespace Lighthouse.Backend.Tests.Services.Implementation.BackgroundServices.Up
 
             await subject.StartAsync(CancellationToken.None);
 
-            Mock.Get(UpdateQueueService).Verify(x => x.EnqueueUpdate(UpdateType.Team, team1.Id, It.IsAny<Func<IServiceProvider, Task>>()));
-            Mock.Get(UpdateQueueService).Verify(x => x.EnqueueUpdate(UpdateType.Team, team2.Id, It.IsAny<Func<IServiceProvider, Task>>()));
+            teamDataServiceMock.Verify(x => x.UpdateTeamData(team1), Times.Once);
+            teamDataServiceMock.Verify(x => x.UpdateTeamData(team2), Times.Once);
         }
 
         [Test]
@@ -71,8 +79,40 @@ namespace Lighthouse.Backend.Tests.Services.Implementation.BackgroundServices.Up
 
             await subject.StartAsync(CancellationToken.None);
 
-            Mock.Get(UpdateQueueService).Verify(x => x.EnqueueUpdate(UpdateType.Team, team1.Id, It.IsAny<Func<IServiceProvider, Task>>()));
-            Mock.Get(UpdateQueueService).Verify(x => x.EnqueueUpdate(UpdateType.Team, team2.Id, It.IsAny<Func<IServiceProvider, Task>>()), Times.Never);
+            teamDataServiceMock.Verify(x => x.UpdateTeamData(team1), Times.Once);
+            teamDataServiceMock.Verify(x => x.UpdateTeamData(team2), Times.Never);
+        }
+
+        [Test]
+        public async Task ExecuteAsync_ShouldBeRefreshed_NoPremiumLicense_MoreThanThreeTeams_DoesNotRefresh()
+        {
+            var team = CreateTeam(DateTime.Now.AddDays(-1));
+
+            SetupRefreshSettings(10, 360);
+
+            SetupTeams([team, CreateTeam(DateTime.Now), CreateTeam(DateTime.Now), CreateTeam(DateTime.Now)]);
+
+            var subject = CreateSubject();
+
+            await subject.StartAsync(CancellationToken.None);
+
+            teamDataServiceMock.Verify(x => x.UpdateTeamData(team), Times.Never);
+        }
+
+        [Test]
+        public async Task ExecuteAsync_ShouldBeRefreshed_PremiumLicense_MoreThanThreeTeams_Refreshes()
+        {
+            var team = CreateTeam(DateTime.Now.AddDays(-1));
+            SetupRefreshSettings(10, 360);
+            SetupTeams([team, CreateTeam(DateTime.Now), CreateTeam(DateTime.Now), CreateTeam(DateTime.Now)]);
+
+            licenseServiceMock.Setup(x => x.CanUsePremiumFeatures()).Returns(true);
+
+            var subject = CreateSubject();
+
+            await subject.StartAsync(CancellationToken.None);
+
+            teamDataServiceMock.Verify(x => x.UpdateTeamData(team), Times.Once);
         }
 
         private void SetupRefreshSettings(int interval, int refreshAfter)
