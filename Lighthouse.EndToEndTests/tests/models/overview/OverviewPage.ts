f683a0d466502e475cf914669f1eb922589dc5ff
Benji Huser-Berta <github.com.bok@huser-berta.com>
2025-09-05 14:26:00 +0200

    Fix failing e2e test

--- Lighthouse.EndToEndTests/tests/models/overview/OverviewPage.ts
+++ Lighthouse.EndToEndTests/tests/models/overview/OverviewPage.ts
@@ -126,6 +126,6 @@ export class OverviewPage {
 	}
 
 	get toolbar(): Locator {
-		return this.page.getByText("LighthouseOverviewTeamsProjectsSettings");
+		return this.page.getByText("LighthouseOverviewSettings");
 	}
 }
