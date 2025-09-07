d1aeac50570a821ddcb94b6cab66716369d46b24
Benji Huser-Berta <github.com.bok@huser-berta.com>
2025-09-07 15:53:49 +0200

    Implement DemoController with dummy data

---
+++ Lighthouse.Backend/Lighthouse.Backend/API/DemoController.cs
@@ -0,0 +1,56 @@
+﻿using Lighthouse.Backend.Models.DemoData;
+using Lighthouse.Backend.Services.Implementation.Licensing;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lighthouse.Backend.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet("scenarios")]
+        public ActionResult<List<DemoDataScenario>> GetScenarios()
+        {
+            var scenarios = new List<DemoDataScenario>();
+
+            scenarios.Add(
+                new DemoDataScenario
+                {
+                    Id = 0,
+                    Title = "Free Scenario",
+                    Description = "Basic Scenario",
+                    NumberOfTeams = 1,
+                    NumberOfProjects = 1,
+                    IsPremium = false,
+                }
+                );
+
+            scenarios.Add(
+                new DemoDataScenario
+                {
+                    Id = 1,
+                    Title = "Premium Scenario",
+                    Description = "Advanced Scenario",
+                    NumberOfTeams = 3,
+                    NumberOfProjects = 2,
+                    IsPremium = true,
+                }
+                );
+
+            return Ok(scenarios);
+        }
+
+        [HttpPost("scenarios/{id}/load")]
+        public ActionResult LoadScenario(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost("scenarios/load-all")]
+        [LicenseGuard(RequirePremium = true)]
+        public ActionResult LoadAll()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Lighthouse.Backend/Lighthouse.Backend/Models/DemoData/DemoDataScenario.cs
@@ -0,0 +1,19 @@
+﻿using Lighthouse.Backend.Services.Interfaces;
+
+namespace Lighthouse.Backend.Models.DemoData
+{
+    public class DemoDataScenario : IEntity
+    {
+        public int Id { get; set; }
+
+        public string Title {  get; set; }
+
+        public string Description { get; set; }
+
+        public int NumberOfTeams {  get; set; }
+
+        public int NumberOfProjects { get; set; }
+
+        public bool IsPremium {  get; set; }
+    }
+}
