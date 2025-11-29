6f820281d046161c7306f8b3e2a2a34b96682c72
Benjamin Huser-Berta <github.com.bok@huser-berta.com>
2025-11-29 14:17:01 +0100

    chore(e2e): switch to pnpm and add pnpm-lock.yaml

--- Lighthouse.EndToEndTests/tests/helpers/screenshots.ts
+++ Lighthouse.EndToEndTests/tests/helpers/screenshots.ts
@@ -1,6 +1,6 @@
 import * as fs from "node:fs";
 import * as path from "node:path";
-import type { Locator, Page } from "playwright";
+import type { Locator, Page } from "@playwright/test";
 import { PNG } from "pngjs";
 import { getPathToDocsAssetsFolder } from "./folderPaths";
 
