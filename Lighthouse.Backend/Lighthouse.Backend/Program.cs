643ae50f47cc9f05bdac0803b7ed02fc7839016a
Benjamin Huser-Berta <github.com.bok@huser-berta.com>
2025-12-20 12:56:17 +0100

    move wwwroot to resources folder in app bundle

--- Lighthouse.Backend/Lighthouse.Backend/Program.cs
+++ Lighthouse.Backend/Lighthouse.Backend/Program.cs
@@ -100,6 +100,14 @@ namespace Lighthouse.Backend
             var dbPath = Path.Combine(userProfile, "Library", "Application Support", "Lighthouse", "LighthouseAppContext.db");
             var connectionString = $"Data Source={dbPath}";
             builder.Configuration["Database:ConnectionString"] = connectionString;
+
+            // Override wwwroot path for app bundle
+            var executablePath = AppContext.BaseDirectory;
+            if (executablePath.Contains(".app/Contents/MacOS"))
+            {
+                var resourcesPath = Path.Combine(executablePath, "..", "Resources", "wwwroot");
+                builder.Environment.WebRootPath = Path.GetFullPath(resourcesPath);
+            }
         }
 
 
