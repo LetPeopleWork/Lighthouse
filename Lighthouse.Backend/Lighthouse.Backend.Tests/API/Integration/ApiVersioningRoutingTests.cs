5cd3d3202f334571fc29e29b95bd29fb3ff9923a
Benjamin Huser-Berta <github.com.bok@huser-berta.com>
2026-04-24 20:46:20 +0200

    Remove obsolete test

--- Lighthouse.Backend/Lighthouse.Backend.Tests/API/Integration/ApiVersioningRoutingTests.cs
+++ Lighthouse.Backend/Lighthouse.Backend.Tests/API/Integration/ApiVersioningRoutingTests.cs
@@ -5,7 +5,6 @@ namespace Lighthouse.Backend.Tests.API.Integration
 {
     public class ApiVersioningRoutingTests() : IntegrationTestBase(new TestWebApplicationFactory<Program>())
     {
-        private const string UnversionedUpdateSupportedPath = "/api/version/updateSupported";
         private const string V1UpdateSupportedPath = "/api/v1/version/updateSupported";
         private const string LatestUpdateSupportedPath = "/api/latest/version/updateSupported";
 
@@ -14,8 +13,11 @@ namespace Lighthouse.Backend.Tests.API.Integration
         {
             var response = await Client.GetAsync(V1UpdateSupportedPath);
 
-            Assert.That(response.IsSuccessStatusCode, Is.True);
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(response.IsSuccessStatusCode, Is.True);
+                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            }
         }
 
         [Test]
@@ -27,22 +29,12 @@ namespace Lighthouse.Backend.Tests.API.Integration
             var latestResponse = await Client.GetAsync(LatestUpdateSupportedPath);
             var latestBody = await latestResponse.Content.ReadAsStringAsync();
 
-            Assert.That(v1Response.IsSuccessStatusCode, Is.True);
-            Assert.That(latestResponse.StatusCode, Is.EqualTo(v1Response.StatusCode));
-            Assert.That(latestBody, Is.EqualTo(v1Body));
-        }
-
-        [Test]
-        public async Task GetUpdateSupported_OnUnversionedRoute_DoesNotMatchVersionedContract()
-        {
-            var v1Response = await Client.GetAsync(V1UpdateSupportedPath);
-            var v1Body = await v1Response.Content.ReadAsStringAsync();
-
-            var unversionedResponse = await Client.GetAsync(UnversionedUpdateSupportedPath);
-            var unversionedBody = await unversionedResponse.Content.ReadAsStringAsync();
-
-            Assert.That(v1Response.IsSuccessStatusCode, Is.True);
-            Assert.That(unversionedBody, Is.Not.EqualTo(v1Body));
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(v1Response.IsSuccessStatusCode, Is.True);
+                Assert.That(latestResponse.StatusCode, Is.EqualTo(v1Response.StatusCode));
+                Assert.That(latestBody, Is.EqualTo(v1Body));
+            }
         }
     }
 }
